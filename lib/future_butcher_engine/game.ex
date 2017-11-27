938a43bd7861daae9c12d7c43dc8b545e1674a93
Jon <jonchoukroun@gmail.com>
2017-11-26 21:38:35 -0800

    Simplify new cut function

--- lib/future_butcher_engine/game.ex
+++ lib/future_butcher_engine/game.ex
@@ -1,3 +1,21 @@
 defmodule FutureButcherEngine.Game do
   alias __MODULE__
+
+  @enforce_keys [:turns_left]
+  defstruct [:turns_left]
+
+  def new(turns) do
+    {:ok, %Game{turns_left: turns}}
+  end
+
+  def end_turn(game) do
+    {:ok, game
+          |> decrement_turn()}
+  end
+
+  defp decrement_turn(game) do
+    game
+    |> Map.put(:turns_left, Map.get(game, :turns_left) - 1)
+  end
+
 end
