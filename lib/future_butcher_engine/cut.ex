073b4a461ff8e27f03a064916cc2337a388615cd
Jon <jonchoukroun@gmail.com>
2017-12-02 14:41:44 -0800

    Pass error message when trying to create new cut with quantity in excess of maximum

--- lib/future_butcher_engine/cut.ex
+++ lib/future_butcher_engine/cut.ex
@@ -4,18 +4,29 @@ defmodule FutureButcherEngine.Cut do
   defstruct [:type, :price]
 
   def new(type, quantity) do
-    {:ok, %Cut{type: type, price: calculate_price(type, quantity)}}
+    # {:ok, %Cut{type: type, price: calculate_price(type, quantity)}}
+
+    case calculate_price(type, quantity) do
+      {:error, msg} ->
+        {:error, msg}
+      cut_price ->
+        {:ok, %Cut{type: type, price: cut_price}}
+    end
   end
 
   defp calculate_price(type, quantity) do
-    values = cut_values(type)
-    round((elem(values, 0) * quantity) + elem(values, 1))
+    case cut_values(type, quantity) do
+      {:error, msg} ->
+        {:error, msg}
+      {slope, max_price} ->
+        round((slope * quantity) + max_price)
+    end
   end
 
-  defp cut_values(:flank), do: {-450.0, 14_000}
-  defp cut_values(:heart), do: {-1000.0, 25_000}
-  defp cut_values(:liver), do: {-0.55, 65}
-  defp cut_values(:loin), do: {-17.0, 1300}
-  defp cut_values(:ribs), do: {-83.0, 3500}
-  defp cut_values(_), do: {:error, :invalid_cut_type}
+  defp cut_values(:flank, quantity) when quantity <= 20, do: {-450.0, 14_000}
+  defp cut_values(:heart, quantity) when quantity <= 10, do: {-1000.0, 25_000}
+  defp cut_values(:liver, quantity) when quantity <= 100, do: {-0.55, 65}
+  defp cut_values(:loin, quantity) when quantity <= 45, do: {-17.0, 1300}
+  defp cut_values(:ribs, quantity) when quantity <= 30, do: {-83.0, 3500}
+  defp cut_values(_, quantity), do: {:error, "#{quantity} exceeds cut maximum"}
 end
