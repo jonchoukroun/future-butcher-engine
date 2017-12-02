a2ef4d31ae69a1a9801752ff76d2c8e9123fff1f
Jon <jonchoukroun@gmail.com>
2017-12-02 15:33:02 -0800

    Define rules for moving between in game state to at market or at subway states; define rules at market and at subway

--- lib/future_butcher_engine/rules.ex
+++ lib/future_butcher_engine/rules.ex
@@ -9,5 +9,33 @@ defmodule FutureButcherEngine.Rules do
     {:ok, %Rules{rules | state: :in_game}}
   end
 
+  def check(%Rules{state: :in_game} = rules, :visit_market) do
+    {:ok, %Rules{rules | state: :at_market}}
+  end
+
+  def check(%Rules{state: :in_game} = rules, :visit_subway) do
+    {:ok, %Rules{rules | state: :at_subway}}
+  end
+
+  def check(%Rules{state: :at_market} = rules, :buy_cut) do
+    {:ok, %Rules{rules | state: :at_market}}
+  end
+
+  def check(%Rules{state: :at_market} = rules, :sell_cut) do
+    {:ok, %Rules{rules | state: :at_market}}
+  end
+
+  def check(%Rules{state: :at_market} = rules, :leave_market) do
+    {:ok, %Rules{rules | state: :in_game}}
+  end
+
+  def check(%Rules{state: :at_subway} = rules, :change_station) do
+    {:ok, %Rules{rules | state: :in_game}}
+  end
+
+  def check(%Rules{state: :at_subway} = rules, :leave_subway) do
+    {:ok, %Rules{rules | state: :in_game}}
+  end
+
   def check(_state, _action), do: :error
 end
