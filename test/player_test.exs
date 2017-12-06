c08addc862c176ad3fc956a41e96bebd75c95178
Jon <jonchoukroun@gmail.com>
2017-12-05 22:10:52 -0800

    Ensure invalid values return error

--- test/player_test.exs
+++ test/player_test.exs
@@ -9,4 +9,10 @@ defmodule FutureButcherEngine.PlayerTest do
   test "Player.new/2 when funds is invalid returns error" do
     assert Player.new(100, 0) == {:error, :invalid_player_values}
   end
+
+  test "Buying with insufficient funds returns error" do
+    {:ok, player} = Player.new(100, 1000)
+    assert Player.adjust_funds(player, 1001, :buy) == {
+      :error, :insufficient_funds}
+  end
 end
