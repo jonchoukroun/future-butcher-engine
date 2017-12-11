39a2e03cf6bc6befdc87564cca5bb72ecdd8e90d
Jon <jonchoukroun@gmail.com>
2017-12-10 19:54:51 -0800

    Fix to match player struct value of new

--- test/player_test.exs
+++ test/player_test.exs
@@ -11,42 +11,42 @@ defmodule FutureButcherEngine.PlayerTest do
   end
 
   test "Buying with insufficient funds returns error" do
-    {:ok, player} = Player.new(100, 1000)
+    player = Player.new(100, 1000)
     assert Player.adjust_funds(player, 1001, :buy) == {
       :error, :insufficient_funds}
   end
 
   test "Buying with sufficient funds returns expected values" do
-    {:ok, player} = Player.new(100, 1000)
+    player = Player.new(100, 1000)
     {:ok, player} = Player.adjust_funds(player, 100, :buy)
     assert player.funds == 900
   end
 
   test "Selling returns expected values" do
-    {:ok, player} = Player.new(100, 1000)
+    player = Player.new(100, 1000)
     {:ok, player} = Player.adjust_funds(player, 100, :sell)
     assert player.funds == 1100
   end
 
   test "Hurting player by more than current health results in death" do
-    {:ok, player} = Player.new(100, 1000)
+    player = Player.new(100, 1000)
     assert Player.adjust_health(player, 200, :hurt) == {:ok, :player_dead}
   end
 
   test "Healing player by more than max health returns max health" do
-    {:ok, player} = Player.new(50, 1000)
+    player = Player.new(50, 1000)
     {:ok, player} = Player.adjust_health(player, 100, :heal)
     assert player.health == 100
   end
 
   test "Hurting player decreases health by expected amount" do
-    {:ok, player} = Player.new(100, 1000)
+    player = Player.new(100, 1000)
     {:ok, player} = Player.adjust_health(player, 20, :hurt)
     assert player.health == 80
   end
 
   test "Healing player increases health by expected amount" do
-    {:ok, player} = Player.new(40, 1000)
+    player = Player.new(40, 1000)
     {:ok, player} = Player.adjust_health(player, 20, :heal)
     assert player.health == 60
   end
