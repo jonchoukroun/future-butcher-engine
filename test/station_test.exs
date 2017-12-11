ab515da65f306835bea8c950a2822149dff526a0
Jon <jonchoukroun@gmail.com>
2017-12-10 16:31:24 -0800

    Ensure invalid station guard returns error

---
+++ test/station_test.exs
@@ -0,0 +1,14 @@
+defmodule FutureButcherEngine.StationTest do
+  use ExUnit.Case
+  alias FutureButcherEngine.Station
+
+  test "Invalid station name returns error" do
+    assert Station.new(:bullshit_name) == {:error, :invalid_station}
+  end
+
+  test "Valid station name creates station with market quantities and values" do
+    {:ok, station} = Station.new(:downtown)
+    assert station.station_name == :downtown
+    assert is_list(station.market)
+  end
+end
