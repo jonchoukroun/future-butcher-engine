a08ff4f3611c8e95ed82a685b7209c13953291fc
Jon <jonchoukroun@gmail.com>
2017-12-11 16:37:06 -0800

    Define start game and visit market public functions with handle call callbacks

--- lib/future_butcher_engine/game.ex
+++ lib/future_butcher_engine/game.ex
@@ -1,6 +1,6 @@
 defmodule FutureButcherEngine.Game do
   use GenServer
-  alias FutureButcherEngine.{Game, Player, Rules}
+  alias FutureButcherEngine.{Game, Player, Station, Rules}
 
   @enforce_keys [:player, :rules]
   defstruct [:player, :rules]
@@ -16,7 +16,49 @@ defmodule FutureButcherEngine.Game do
   def init(game_rules) do
     {:ok, %{
       rules: Rules.new(game_rules.turns),
-      player: Player.new(game_rules.health, game_rules.funds)}}
+      player: Player.new(game_rules.health, game_rules.funds),
+      station: nil}}
   end
+
+  def start_game(game) do
+    GenServer.call(game, :start_game)
+  end
+
+  def visit_market(game) do
+    GenServer.call(game, :visit_market)
+  end
+
+  def handle_call(:start_game, _from, state_data) do
+    with {:ok, rules} <- Rules.check(state_data.rules, :start_game)
+    do
+      state_data
+      |> update_rules(rules)
+      |> travel_to(:downtown)
+      |> reply_success(:ok)
+    else
+      :error -> {:reply, :error, state_data}
+    end
+  end
+
+  def handle_call(:visit_market, _from, state_data) do
+    with {:ok, rules} <- Rules.check(state_data.rules, :visit_market)
+    do
+      state_data
+      |> update_rules(rules)
+      |> reply_success(:ok)
+    else
+      :error -> {:reply, :error, state_data}
+    end
+  end
+
+  defp current_market(game), do: Map.get(state_data, :station).market
+
+  defp update_rules(state_data, rules), do: %{state_data | rules: rules}
+
+  defp travel_to(state_data, station) do
+    %{state_data | station: Station.new(station)}
+  end
+
+  defp reply_success(state_data, reply), do: {:reply, reply, state_data}
 
 end
