36b3fb3b0ff32ec96c82e4a471c2fc9fff0593f8
Jon <jonchoukroun@gmail.com>
2017-12-18 21:37:42 -0800

    Update market by accessing station and market values then returning state data

--- lib/future_butcher_engine/game.ex
+++ lib/future_butcher_engine/game.ex
@@ -24,14 +24,6 @@ defmodule FutureButcherEngine.Game do
     GenServer.call(game, :start_game)
   end
 
-  def visit_market(game) do
-    GenServer.call(game, :visit_market)
-  end
-
-  def leave_market(game) do
-    GenServer.call(game, :leave_market)
-  end
-
   def handle_call(:start_game, _from, state_data) do
     with {:ok, rules} <- Rules.check(state_data.rules, :start_game)
     do
@@ -42,6 +34,10 @@ defmodule FutureButcherEngine.Game do
     else
       :error -> {:reply, :error, state_data}
     end
+  end
+
+  def visit_market(game) do
+    GenServer.call(game, :visit_market)
   end
 
   def handle_call(:visit_market, _from, state_data) do
@@ -55,6 +51,10 @@ defmodule FutureButcherEngine.Game do
     end
   end
 
+  def leave_market(game) do
+    GenServer.call(game, :leave_market)
+  end
+
   def handle_call(:leave_market, _from, state_data) do
     with {:ok, rules} <- Rules.check(state_data.rules, :leave_market)
     do
@@ -66,7 +66,55 @@ defmodule FutureButcherEngine.Game do
     end
   end
 
-  defp current_market(game), do: Map.get(state_data, :station).market
+  def buy_cut(game, cut, amount) do
+    GenServer.call(game, {:buy_cut, cut, amount})
+  end
+
+  def handle_call({:buy_cut, cut, amount}, _from, state_data) do
+    with {:ok, rules}  <- Rules.check(state_data.rules, :buy_cut),
+         {:ok, price}  <- get_price(state_data.station.market, cut, amount),
+         {:ok, player} <- Player.buy_cut(state_data.player, cut, amount, price)
+    do
+      state_data
+      |> update_market(cut, amount, :buy)
+      # |> update_player(player)
+      # |> update_rules(rules)
+      |> reply_success(:ok)
+    else
+      :error -> {:reply, :error, state_data}
+    end
+  end
+
+  defp get_price(market, cut, amount) do
+    case valid_amount?(market, cut, amount) do
+      {:ok} ->
+        {:ok, Map.get(market, cut).price * amount}
+      {:error, msg} ->
+        {:error, msg}
+    end
+  end
+
+  defp valid_amount?(market, cut, amount) do
+    if Map.get(market, cut).quantity > amount do
+      {:ok}
+    else
+      {:error, :invalid_amount}
+    end
+  end
+
+  defp update_market(state_data, cut, amount, transaction_type) do
+    amount = if (transaction_type == :buy), do: amount * -1, else: amount
+
+    put_in(state_data, access_cut_value(cut, :quantity),
+      Map.get(state_data.station.market, cut).quantity + amount)
+  end
+
+  defp access_cut_value(cut, k)
+  when k in [:price, :quantity] do
+    [Access.key(:station), Access.key(:market), Access.key(cut), Access.key(k)]
+  end
+
+  defp update_player(state_data, player), do: %{state_data | player: player}
 
   defp update_rules(state_data, rules), do: %{state_data | rules: rules}
 
