163b794abb30c6e910761c3e0db555f1df5664a4
Jon <jonchoukroun@gmail.com>
2018-02-03 19:54:06 -0800

    Define custom start link and init functions

--- lib/future_butcher_engine/game_supervisor.ex
+++ lib/future_butcher_engine/game_supervisor.ex
@@ -2,4 +2,10 @@ defmodule FutureButcherEngine.GameSupervisor do
   use Supervisor
   alias FutureButcherEngine.Game
 
+  def start_link(_options) do
+    Supervisor.start_link(__MODULE__, :ok, name: __MODULE__)
+  end
+
+  def init(:ok), do: Supervisor.init([Game], strategy: :simple_one_for_one)
+
 end
