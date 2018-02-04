f80101bb6b1d3423b0efb5c4305f80e887247a3a
Jon <jonchoukroun@gmail.com>
2018-02-03 19:00:20 -0800

    Supervise Registry module and require unique keys

--- lib/future_butcher_engine/application.ex
+++ lib/future_butcher_engine/application.ex
@@ -8,8 +8,7 @@ defmodule FutureButcherEngine.Application do
   def start(_type, _args) do
     # List all child processes to be supervised
     children = [
-      # Starts a worker by calling: FutureButcherEngine.Worker.start_link(arg)
-      # {FutureButcherEngine.Worker, arg},
+      {Registry, keys: :unique, name: Registry.Game}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
