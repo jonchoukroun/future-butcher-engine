b376e566fa1d75695a9b4b6bb88474ed7602c70f
Jon <jonchoukroun@gmail.com>
2018-06-12 20:34:08 -0700

    Set turns to 24

--- lib/future_butcher_engine/game.ex
+++ lib/future_butcher_engine/game.ex
@@ -8,7 +8,7 @@ defmodule FutureButcherEngine.Game do
   @stations [:downtown, :venice_beach, :koreatown, :culver_city, :silverlake]
 
   # Hardcoded to allow game/player creation from player_name arg only
-  @turns 10
+  @turns 24
   @health 100
   @funds 5000
 
