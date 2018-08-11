fb91ba725cecda5ededb44ce9ce2e232c78ae9e9
Jon <jonchoukroun@gmail.com>
2018-08-11 16:50:49 -0700

    Refactor

--- test/player_test.exs
+++ test/player_test.exs
@@ -55,19 +55,27 @@ defmodule FutureButcherEngine.PlayerTest do
     end
   end
 
-  test "Debt accrues on debt by rate rate" do
-    player = Player.new("Frank")
-    debt   = 5000
-    rate   = 0.5
-    {:ok, player} = Player.buy_loan(player, debt, rate)
-    {:ok, player} = Player.accrue_debt(player)
+  describe ".accrue_debt" do
+    setup _context do
+      %{player: Player.new("Frank")}
+    end
 
-    assert player.debt == debt * (1 + rate)
+    test "with no debt does nothing", context do
+      {:ok, test_player} = Player.accrue_debt(context.player)
+      assert test_player == context.player
+    end
+
+    test "with debt raises debt by interest rate amount", context do
+      {:ok, player} = Player.buy_loan(context.player, 5000, 0.2)
+      {:ok, test_player} = Player.accrue_debt(player)
+      assert test_player.debt == 6000
+    end
+
   end
 
   describe ".buy_cut" do
     setup _context do
-      {:ok, player} = Player.new("Frank") |> adjust_funds(player, :increase, 5000)
+      {:ok, player} = Player.new("Frank") |> Player.adjust_funds(:increase, 5000)
       %{player: player}
     end
 
@@ -75,98 +83,79 @@ defmodule FutureButcherEngine.PlayerTest do
       assert Player.buy_cut(context.player, :loin, 4, 10_000) == {:error, :insufficient_funds}
     end
 
+    test "with insufficient space returns error", context do
+      assert Player.buy_cut(context.player, :ribs, 100, 3000) == {:error, :insufficient_pack_space}
+    end
+
+    test "with valid args increases cuts and decreases funds", context do
+      {:ok, test_player} = Player.buy_cut(context.player, :heart, 10, 1000)
+      assert test_player.pack.heart == 10
+      assert test_player.funds      == 4000
+    end
+
   end
 
-  # test "Buying with insufficient funds returns error" do
-  #   player = Player.new("Frank")
-  #   assert Player.buy_cut(player, :ribs, 20, 1300) == {:error, :insufficient_funds}
-  # end
-  #
-  # test "Buying with insufficient space returns error" do
-  #   {:ok, player} = Player.buy_loan(Player.new("Frank"), 5000, 0.5)
-  #   assert Player.buy_cut(player, :ribs, 100, 1) == {:error, :insufficient_pack_space}
-  #
-  #   {:ok, player} = Player.buy_cut(player, :ribs, 10, 10)
-  #   assert Player.buy_cut(player, :ribs, 11, 10) == {:error, :insufficient_pack_space}
-  # end
-  #
-  # test "Valid purchase increases owned cut and decreases funds" do
-  #   {:ok, player} = Player.buy_loan(Player.new("Frank"), 5000, 0.5)
-  #   {:ok, player} = Player.buy_cut(player, :loin, 5, 100)
-  #   assert Map.get(player.pack, :loin) == 5
-  #   assert player.funds == 4900
-  # end
-  #
-  # test "Selling more cuts than owned returns error" do
-  #   {:ok, player} = Player.buy_loan(Player.new("Frank"), 5000, 0.5)
-  #   {:ok, player} = Player.buy_cut(player, :heart, 5, 10)
-  #   assert Player.sell_cut(player, :heart, 6, 10) == {:error, :insufficient_cuts}
-  # end
-  #
-  # test "Valid sale decreases owned cut and increases funds" do
-  #   {:ok, player} = Player.buy_loan(Player.new("Frank"), 5000, 0.5)
-  #   {:ok, player} = Player.buy_cut(player, :flank, 5, 10)
-  #   {:ok, player} = Player.sell_cut(player, :flank, 3, 20)
-  #   assert Map.get(player.pack, :flank) == 2
-  #   assert player.funds == 5010
-  # end
-  #
-  # test "Decreasing fund by more than available zeroes out funds" do
-  #   {:ok, player} = Player.buy_loan(Player.new("Frank"), 5000, 0.5)
-  #   {:ok, player} = Player.adjust_funds(player, :decrease, 8000)
-  #   assert player.funds == 0
-  # end
-  #
-  # test "Decreasing funds by less than available reduces funds" do
-  #   {:ok, player} = Player.buy_loan(Player.new("Frank"), 5000, 0.5)
-  #   {:ok, player} = Player.adjust_funds(player, :decrease, 500)
-  #   assert player.funds == 4500
-  # end
-  #
-  # test "Increasing funds raises player's funds by amount" do
-  #   {:ok, player} = Player.buy_loan(Player.new("Frank"), 5000, 0.5)
-  #   {:ok, player} = Player.adjust_funds(player, :increase, 500)
-  #   assert player.funds == 5500
-  # end
-  #
-  # test "Debt doesn't accrue when paid off" do
-  #   {:ok, player} = Player.buy_loan(Player.new("Frank"), 5000, 0.5)
-  #   assert player.debt > 0
-  #
-  #   {:ok, player} = Player.adjust_funds(player, :increase, 1000)
-  #   {:ok, player} = Player.pay_debt(player, player.debt)
-  #
-  #   assert Player.accrue_debt(player) == {:ok, player}
-  #   assert player.debt == 0
-  # end
-  #
-  # test "Paying debt with amount less debt reduces funds and debt but keeps rate" do
-  #   {:ok, player} = Player.buy_loan(Player.new("Frank"), 5000, 0.5)
-  #
-  #   {:ok, player} = Player.pay_debt(player, 1000)
-  #   assert player.debt  == 4000
-  #   assert player.funds == 4000
-  #   assert player.rate  == 0.5
-  # end
-  #
-  # test "Paying debt in full clears debt and rate rate" do
-  #   {:ok, player} = Player.buy_loan(Player.new("Frank"), 5000, 0.5)
-  #   {:ok, player} = Player.adjust_funds(player, :increase, 1000)
-  #
-  #   {:ok, player} = Player.pay_debt(player, 5000)
-  #   assert player.funds == 1000
-  #   assert player.debt  == 0
-  #   assert player.rate  == 0.0
-  # end
-  #
-  # test "Paying debt with amount greater than funds returns error" do
-  #   {:ok, player} = Player.buy_loan(Player.new("Frank"), 5000, 0.5)
-  #   {:ok, player} = Player.adjust_funds(player, :decrease, 100)
-  #   refute player.funds > player.debt
-  #
-  #   amount = player.funds
-  #
-  #   assert Player.pay_debt(player, amount) == {:error, :insufficient_funds}
-  # end
+  describe ".sell_cut" do
+    setup _context do
+      {:ok, player} = Player.new("Frank") |> Player.adjust_funds(:increase, 2000)
+      {:ok, player} = Player.buy_cut(player, :ribs, 5, 500)
+      %{player: player}
+    end
 
+    test "with more cuts than owned", context do
+      assert Player.sell_cut(context.player, :ribs, 6, 500) == {:error, :insufficient_cuts}
+    end
+
+    test "with valid args decreases cut and increases funds", context do
+      {:ok, test_player} = Player.sell_cut(context.player, :ribs, 5, 1000)
+
+      assert test_player.pack.ribs == 0
+      assert test_player.funds     == 2500
+    end
+  end
+
+  describe ".adjust_funds" do
+    setup _context do
+      {:ok, player} = Player.new("Frank") |> Player.buy_loan(1000, 0.1)
+      %{player: player}
+    end
+
+    test "decreasing by more than availabled funds zeroes player funds", context do
+      {:ok, test_player} = Player.adjust_funds(context.player, :decrease, 2000)
+      assert test_player.funds == 0
+    end
+
+    test "decreasing by less than funds returns expected amount", context do
+      {:ok, test_player} = Player.adjust_funds(context.player, :decrease, 500)
+      assert test_player.funds == 500
+    end
+
+    test "increasing raises player funds", context do
+      {:ok, test_player} = Player.adjust_funds(context.player, :increase, 1000)
+      assert test_player.funds == 2000
+    end
+
+  end
+
+  describe ".pay_debt" do
+    setup _context do
+      {:ok, player} = Player.new("Frank") |> Player.buy_loan(1000, 0.1)
+      {:ok, player} = Player.adjust_funds(player, :increase, 2000)
+      %{player: player}
+    end
+
+    test "with full amount clears debt and rate", context do
+      {:ok, test_player} = Player.pay_debt(context.player, 1000)
+      assert test_player.funds == 2000
+      assert test_player.debt  == 0
+      assert test_player.rate  == 0.0
+    end
+
+    test "with amount greater than funds returns error", context do
+      {:ok, test_player} = Player.adjust_funds(context.player, :decrease, 2500)
+      assert Player.pay_debt(test_player, 1000) == {:error, :insufficient_funds}
+    end
+
+  end
+  
 end
