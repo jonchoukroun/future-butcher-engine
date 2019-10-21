2e49851a9ca4ddbafad9ad84d4ad208639a3d240
Jon <jonchoukroun@gmail.com>
2019-10-21 08:32:31 -0700

    Increment patch version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule FutureButcherEngine.Mixfile do
   def project do
     [
       app: :future_butcher_engine,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.9.2",
       description: description(),
       package: package(),
