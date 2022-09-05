3f20384b6b38205cdb8327b1820260a5a9173e53
Jon <jonchoukroun@gmail.com>
2022-09-04 23:36:21 -0700

    Restore patch number

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule FutureButcherEngine.Mixfile do
   def project do
     [
       app: :future_butcher_engine,
-      version: "1.0.4",
+      version: "1.0.3",
       elixir: "~> 1.13.4",
       description: description(),
       package: package(),
