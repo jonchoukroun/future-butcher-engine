928b48760e3464d46b60100b94628d23f8a575d6
Jon <jonchoukroun@gmail.com>
2022-09-07 01:06:39 -0700

    Redfine weapon price function

--- lib/future_butcher_engine/weapon.ex
+++ lib/future_butcher_engine/weapon.ex
@@ -12,14 +12,16 @@ defmodule FutureButcherEngine.Weapon do
 
   def weapon_types, do: @weapons_list
 
-  def generate_price(weapon, turns_left) when weapon in @weapons_list do
-    harvest_offset = if can_harvest(weapon), do: 1.2, else: 1
-    damage_discount = 10 - get_damage(weapon) |> Kernel.*(16_000)
-    (100_000 - damage_discount)
-    |> Kernel./(turns_left + 1)
-    |> Kernel.*(22 * harvest_offset )
-    |> round()
-  end
+  def generate_price(weapon, turns_left)
+    when weapon in @weapons_list do
+      time = Station.store_close() - Station.store_open()
+      min = get_in(@weapons, [weapon, :min])
+      max = get_in(@weapons, [weapon, :max])
+      slope = (max - min) / time
+      intercept = max - (slope * Station.store_close())
+
+      (slope * turns_left + intercept) |> round()
+    end
 
   def generate_price(_weapon, _turns_left), do: {:error, :invalid_weapon_type}
 
